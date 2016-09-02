06d8e35089815fca542eb730794e1ba9a1d22852
Mikulas <rullaf@gmail.com>
2016-09-02 14:32:23 +0200

    fix closure from callable on expr callables

--- src/Mikulas/Transpiler/Visitors/RewriteClosureFromCallable.php
+++ src/Mikulas/Transpiler/Visitors/RewriteClosureFromCallable.php
@@ -3,13 +3,13 @@
 namespace Mikulas\Transpiler\Modifiers;
 
 use PhpParser\Node;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor;
+use PhpParser\NodeVisitorAbstract;
 
 
 class RewriteClosureFromCallable extends NodeFilteringVisitor
 {
-
-	const ARGS_NAME = 'args';
-
 
 	public function filter(Node $node): bool
 	{
@@ -29,18 +29,25 @@ class RewriteClosureFromCallable extends NodeFilteringVisitor
 		/** @var Node\Arg $callable */
 		$callable = $node->args[0];
 
-		$param = new Node\Param(self::ARGS_NAME);
-		$param->variadic = TRUE;
-		$closure->params = [$param];
+		// extract all variables for use
+		$traverser = new NodeTraverser();
+		$traverser->addVisitor(new class($closure) extends NodeVisitorAbstract {
+			/** @var Node\Expr\Closure */
+			private $closure;
 
-		if ($callable->value instanceof Node\Expr\Array_) {
-			$value = $callable->value->items[0]->value;
-			if ($value instanceof Node\Expr\Variable) {
-				$closure->uses[] = new Node\Expr\ClosureUse($value->name, TRUE);
+			public function __construct(Node\Expr\Closure $closure) {
+				$this->closure = $closure;
 			}
-		}
 
-		$arg = new Node\Arg(new Node\Expr\Variable(self::ARGS_NAME));
+			public function enterNode(Node $node) {
+				if ($node instanceof Node\Expr\Variable) {
+					$this->closure->uses[] = new Node\Expr\ClosureUse($node->name, TRUE);
+				}
+			}
+		});
+		$traverser->traverse($node->args);
+
+		$arg = new Node\Arg(new Node\Expr\FuncCall(new Node\Name('func_get_args')));
 
 		$call = new Node\Expr\FuncCall(new Node\Name('call_user_func_array'), [$callable, $arg]);
 		$closure->stmts[] = new Node\Stmt\Return_($call);
