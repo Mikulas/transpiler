68c4b2a39cb26c6e19259acc858e40f10500fbc6
Mikulas <rullaf@gmail.com>
2016-09-09 10:45:57 +0200

    fix autoload

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types = 1);
 
-require __DIR__ . '/vendor/autoload.php';
+$loader = require __DIR__ . '/vendor/autoload.php';
+$loader->add('Mikulas\\TranspilerBuild\\', __DIR__ . '/src');
 
 $application = new \Mikulas\TranspilerBuild\Application();
 $application->run();
