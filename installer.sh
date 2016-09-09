37cb33f2c98e64877fc50a0ea33f5e95da216b14
Mikulas <rullaf@gmail.com>
2016-09-09 15:36:20 +0200

    release v1.1.2

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 set -euvo pipefail
 IFS=$'\n\t'
 
-VERSION="v1.1.1"
+VERSION="v1.1.2"
 
 TARGET_DIR="/usr/local/bin"
 TARGET="$TARGET_DIR/php-transpiler"
--- src/Mikulas/Transpiler/Transpiler.php
+++ src/Mikulas/Transpiler/Transpiler.php
@@ -11,7 +11,7 @@ use PhpParser\NodeVisitor as PhpParserVisitors;
 class Transpiler
 {
 
-	const VERSION = '1.1.1';
+	const VERSION = '1.1.2';
 
 	/** @var NodeTraverser */
 	private $traverser;
